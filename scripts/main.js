c7e4e2192e5823b5721555b698d3925dc987fed9
Will H <will.wkhu@gmail.com>
2022-01-21 00:02:13 -0800

    Refactor code into factory functions and IIFEs

--- scripts/main.js
+++ scripts/main.js
@@ -1,96 +1,116 @@
-// const gameBoard = document.querySelector('#board-container');
-
-// // if wrapped in IIFE will auto set gameboard
-// // const board = (() => {
-// //   for (let i = 0; i < 3; i++) {
-// //     for (let j = 0; j < 3; j++) {
-// //       const grid = document.createElement('div');
-// //       grid.classList.add('grid');
-// //       grid.id = i + '-' + j;
-// //       gameBoard.appendChild(grid);
-
-
-// //     }
-// //   }
-// // })();
-
-// let arr = [,,,,,,,,,];
-// for (let i = 0; i < arr.length; i++) {
-//   const grid = document.createElement('div');
-//   grid.classList.add('grid');
-//   grid.id = i ;
-//   gameBoard.appendChild(grid);
-// }
-
-
-// let grid = document.querySelectorAll('.grid');
-// // const start = document.querySelector('#start');
-// // // currently must click start to set gameboard
-// // start.addEventListener('click', () => {
-// //   // console.log(gameBoard);
-// //   // console.log({gameBoard});
-// //   // console.log(grid);
-// //   // board();
-
-// //   console.log(grid);
-
-// // });
-
-// grid.forEach((box) => {
-//   box.addEventListener('click', () => {
-//     box.textContent = 'O';
-//   })
-// })
-
-const gameBoard = document.querySelector('#board-container');
-let arr = [,,,,,,,,,];
-for (let i = 0; i < arr.length; i++) {
-  const grid = document.createElement('div');
-  grid.classList.add('grid');
-  grid.id = i ;
-  gameBoard.appendChild(grid);
+const player = (name, mark) => {
+  const movesArr = [];
+  return { name, mark, movesArr }
 }
 
-let winConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8], 
-  [2, 4, 6] 
-]
-let playerXArr = [];
-let playerOArr = [];
 
-let grid = document.querySelectorAll('.grid');
-let counter = 0;
-grid.forEach(box => {
-  box.addEventListener('click', (e) => { 
-    if (counter < 9) {
+const gameboard = (() => {
+  const movesArr = [];
+  const gameboard = document.querySelector('#board-container');
 
-      if (counter % 2 === 0) { // even #; player X turn
-        box.textContent = 'X';
-        playerXArr.push(e.target.id);
-      } else { // odd #; player O turn
-        box.textContent = 'O'; 
-        playerOArr.push(e.target.id);
+  for (let i = 0; i < 9; i++) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    grid.id = i ;
+    gameboard.appendChild(grid);
+  }
+  return { movesArr };
+})();
+
+
+const checkWinner = (() => {
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  const isWinner = (arr) => {
+    return winConditions.some((eachArr) => {
+      return eachArr.every((num) => arr.includes(num));
+    });
+  };
+
+  return { isWinner };
+})();
+
+
+const game = (() => {
+  const playerX = player('player X', 'X');
+  const playerO = player('player O', 'O');
+
+  // start conditions
+  let activePlayer = playerX;
+  let counter = 0;
+
+  const grid = document.querySelectorAll('.grid');
+  grid.forEach(box => {
+    box.addEventListener('click', (e) => {
+      if (counter < 9) {
+        box.textContent = activePlayer.mark;
+        activePlayer.movesArr.push(+e.target.id);
+        console.log(checkWinner.isWinner(activePlayer.movesArr));
+
+        counter++;
+        activePlayer === playerX ? activePlayer = playerO : activePlayer = playerX;
+        
+        // remove eventlistener from marked cells
+        e.target.style.pointerEvents = 'none';
       }
-
-      winConditions.map(arr => {
-        if (arr.every(num => playerXArr.includes(num.toString()))) {
-          console.log('yesss');
-        } else if (arr.every(num => playerOArr.includes(num.toString()))) {
-          console.log('noooo');
-        } else if (counter === 8) {
-          console.log('Tiiieee');
-        }
-      });
-
-
-    };
-    counter++;
+    })
   });
-});
 
+  // const playGame = (mark, element) => {
+  //   if (counter < 9) {
+  //     element.textContent = mark;
+  //     counter++;
+  //     activePlayer === playerX ? activePlayer = playerO : activePlayer = playerX;
+
+  //   }
+  // }
+
+  // const grid = document.querySelectorAll('.grid');
+  // grid.forEach(box => {
+  //   box.addEventListener('click', (e) => {
+  //     playGame(activePlayer.mark, box);
+  //     // e.target.removeEventListener('click');
+  //     console.log(box);
+  //   });
+  // })
+
+  // function markBox(sign, elem) {
+  //   if (counter < 9) {
+  //     elem.textContent = sign;
+  //     counter++;
+  //   }
+  // }
+
+
+
+ 
+  // grid.forEach(box => {
+  //   box.addEventListener('click', (e) => {
+  //     markBox(activePlayer.mark, box);
+  //     activePlayer === playerX ? activePlayer = playerO : activePlayer = playerX;
+  //     e.target.removeEventListener('click', markBox);
+  //   });
+  // })
+
+  // grid.forEach(box => {
+  //   box.addEventListener('click', (e) => {
+  //     if (counter < 9) {
+  //       box.textContent = activePlayer.mark;
+  //       counter++;
+  //       activePlayer === playerX ? activePlayer = playerO : activePlayer = playerX;
+  //       e.target.removeEventListener('click');
+  //     }
+
+  //   })
+  // });
+
+})();
