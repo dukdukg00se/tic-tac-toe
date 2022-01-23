9a751e9300dc573ae30ba9531e2d171f91f5f9cf
Will H <will.wkhu@gmail.com>
2022-01-22 23:49:59 -0800

    Add comments, reset function to board module

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,31 @@
+// player factory
 const player = (name, mark) => {
   const movesArr = [];
   return { name, mark, movesArr }
 }
 
-
-const gameboard = (() => {
-  const movesArr = [];
+// make gameboard
+const board = (() => {
   const gameboard = document.querySelector('#board-container');
-
   for (let i = 0; i < 9; i++) {
     const grid = document.createElement('div');
     grid.classList.add('grid');
     grid.id = i ;
     gameboard.appendChild(grid);
   }
-  return { movesArr };
+
+  const reset = () => {
+    const grid = document.querySelectorAll('.grid');
+    grid.forEach(box => {
+      box.textContent = '';
+    })   
+  };
+
+  return { reset };
 })();
 
 
+// check for winner
 const checkWinner = (() => {
   const winConditions = [
     [0, 1, 2],
@@ -40,13 +48,13 @@ const checkWinner = (() => {
 })();
 
 
-const game = (() => {
-  const playerX = player('player X', 'X');
-  const playerO = player('player O', 'O');
-
+const gamePlay = (() => {
+  const playerX = player('Player X', 'X');
+  const playerO = player('Player O', 'O');
   // start conditions
   let activePlayer = playerX;
   let counter = 0;
+  let gameWon = false;
 
   const grid = document.querySelectorAll('.grid');
   grid.forEach(box => {
@@ -54,63 +62,81 @@ const game = (() => {
       if (counter < 9) {
         box.textContent = activePlayer.mark;
         activePlayer.movesArr.push(+e.target.id);
-        console.log(checkWinner.isWinner(activePlayer.movesArr));
-
-        counter++;
-        activePlayer === playerX ? activePlayer = playerO : activePlayer = playerX;
-        
+        // console.log(checkWinner.isWinner(activePlayer.movesArr));
+        // counter++;
+        // activePlayer === playerX ? activePlayer = playerO : activePlayer = playerX;
         // remove eventlistener from marked cells
+
         e.target.style.pointerEvents = 'none';
       }
-    })
+
+      if (!checkWinner.isWinner(activePlayer.movesArr)) {
+        counter++;
+        activePlayer === playerX ? activePlayer = playerO : activePlayer = playerX;
+      } else {
+        gameWon = true;
+        console.log(gameWon);
+        const modal = document.querySelector('#modal');
+        const modalHeader = document.querySelector('#modal-header');
+        modalHeader.textContent = `${activePlayer.name} Wins!`
+        modal.style.display = 'block';
+
+      }
+
+      console.log(gameWon, counter);
+
+    });
   });
 
-  // const playGame = (mark, element) => {
-  //   if (counter < 9) {
-  //     element.textContent = mark;
-  //     counter++;
-  //     activePlayer === playerX ? activePlayer = playerO : activePlayer = playerX;
-
-  //   }
-  // }
-
-  // const grid = document.querySelectorAll('.grid');
-  // grid.forEach(box => {
-  //   box.addEventListener('click', (e) => {
-  //     playGame(activePlayer.mark, box);
-  //     // e.target.removeEventListener('click');
-  //     console.log(box);
-  //   });
-  // })
-
-  // function markBox(sign, elem) {
-  //   if (counter < 9) {
-  //     elem.textContent = sign;
-  //     counter++;
-  //   }
-  // }
-
-
-
- 
-  // grid.forEach(box => {
-  //   box.addEventListener('click', (e) => {
-  //     markBox(activePlayer.mark, box);
-  //     activePlayer === playerX ? activePlayer = playerO : activePlayer = playerX;
-  //     e.target.removeEventListener('click', markBox);
-  //   });
-  // })
-
-  // grid.forEach(box => {
-  //   box.addEventListener('click', (e) => {
-  //     if (counter < 9) {
-  //       box.textContent = activePlayer.mark;
-  //       counter++;
-  //       activePlayer === playerX ? activePlayer = playerO : activePlayer = playerX;
-  //       e.target.removeEventListener('click');
-  //     }
-
-  //   })
-  // });
+  // console.log(gameWon);
+  return {
+    activePlayer,
+    counter,
+    gameWon
+  };
 
 })();
+
+
+// const closeIt = (e) => {
+//   if (e.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
+
+const displayController = (() => {
+
+
+
+  const closeModal = () => {
+    // modal.style.display = 'none';
+    // console.log(modal);
+  }
+  return { closeModal };
+
+
+})();
+
+// window.addEventListener('click', displayController.closeModal());
+
+// const closeBtn = document.querySelector('.close');
+// closeBtn.addEventListener('click', () => {
+//   modal.style.display = "none";
+
+//   let board = document.querySelector('#board-container');
+//   board.style.pointerEvents = 'none';
+// });
+
+// let reset = document.querySelector('#reset-btn');
+// reset.addEventListener('click', (e) => {
+//   console.log(e);
+//   let parent = document.querySelector('#board-container');
+//   removeAllChildNodes(parent); 
+//   makeBoard.setboard();
+// });
+
+// function removeAllChildNodes(parent) {
+//   while (parent.firstChild) {
+//     parent.removeChild(parent.firstChild);
+//   }
+// }
