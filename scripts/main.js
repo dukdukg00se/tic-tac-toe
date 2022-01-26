75cbfc81c6f3ad2e7fea7b94cfccb021b0c08734
Will H <will.wkhu@gmail.com>
2022-01-25 23:09:28 -0800

    Add comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,40 @@
-// player factory function
+// factory function to create new player objects
 const player = (name, mark) => {
   const moves = [];
-  return { name, mark, moves }
-}
+  return { name, mark, moves };
+};
 
 // gameboard module to set and control board during play
 const board = (() => {
   const gameBoard = document.querySelector('#board-container');
-  // set board to start game
+  // set board at start
   for (let i = 0; i < 9; i++) {
     const grid = document.createElement('div');
     grid.classList.add('grid');
     grid.id = i ;
     gameBoard.appendChild(grid);
   }
-  // erase player marks function
+  // function to erase player marks
   const reset = () => {
     const grid = document.querySelectorAll('.grid');
     grid.forEach(box => {
       box.textContent = '';
       box.style.pointerEvents = '';
-    })   
+    });   
   };
-  // disable board for play
+  // disable board to prevent further moves
   const disableBoard = () => {
     gameBoard.style.pointerEvents = 'none';
-  }
+  };
   // enable board for play 
   const enableBoard = () => {
     gameBoard.style.pointerEvents = '';
-  }
+  };
   return { reset, disableBoard, enableBoard };
 })();
 
-
-const gamePlay = (() => {
+// main module for game play
+const gamePlayController = (() => {
   // set players
   const playerX = player("Player X", "X");
   const playerO = player("Player O", "O");
@@ -43,7 +43,7 @@ const gamePlay = (() => {
   let counter = 0;
   let gameOver = false;
   let winner = false;
-  // function to check for a winner
+  // function to check if win conditions met
   const checkWinner = (moves) => {
     const winConditions = [
       [0, 1, 2],
@@ -55,11 +55,11 @@ const gamePlay = (() => {
       [0, 4, 8],
       [2, 4, 6]
     ];
-    return winConditions.some((eachArr) => {
-      return eachArr.every((num) => moves.includes(num));
+    return winConditions.some((condition) => {
+      return condition.every((num) => moves.includes(num));
     });
   };
-
+  // control gameflow and marks
   const play = (e) => {
     if (counter < 9) {
       e.target.textContent = activePlayer.mark;
@@ -86,12 +86,7 @@ const gamePlay = (() => {
       ? (activePlayer = playerO)
       : (activePlayer = playerX);
   };
-
-  const grid = document.querySelectorAll(".grid");
-  grid.forEach((box) => {
-    box.addEventListener("click", play);
-  });
-
+  // reset variables and board to start conditions
   const resetGame = () => {
     if (!displayController.playerX.classList.contains('active')) {
       displayController.playerX.classList.toggle('active');
@@ -107,28 +102,33 @@ const gamePlay = (() => {
     board.enableBoard();
   };
 
+  const grid = document.querySelectorAll(".grid");
+  grid.forEach((box) => {
+    box.addEventListener("click", play);
+  });
+
   let reset = document.querySelector('#reset-btn');
   reset.addEventListener('click', resetGame);
 })();
 
-
-
+// module to control modal and active player indicators
 const displayController = (() => {
   const modal = document.querySelector('#modal');
   const modalHeader = document.querySelector('#modal-header');
   const closeBtn = document.querySelector('.close');
   const playerX = document.querySelector('#player-x');
   const playerO = document.querySelector('#player-o');
-
+  // function indicates player's turn by highlighting player button
   const showActivePlayer = () => {
     playerX.classList.toggle('active');
     playerO.classList.toggle('active');
-  }
+  };
+  // open modal and set text to declare winner or tie
   const modalController = (winnerDeclared, winnerName) => {
     winnerDeclared ? modalHeader.textContent = `${winnerName} Wins!` : modalHeader.textContent = "Its a Tie!";
     modal.style.display = "block";
-  }
-
+  };
+  // close modal
   closeBtn.addEventListener('click', () => {
     modal.style.display = "none";
   });
